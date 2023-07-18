6b6f58775dc025209183e7ec823b3497c1020ebd
Alessandro Fugazza <137590109+alessandrofugazza@users.noreply.github.com>
2023-07-18 10:32:30 +0200

    adds timer

--- ale.js
+++ ale.js
@@ -1,1 +1,15 @@
-console.log("test");
+const timer = document.querySelector("header div p:nth-of-type(2)");
+
+let secondsRemaining = 60;
+
+timer.innerText = secondsRemaining;
+
+const decreaseTimer = () => {
+  secondsRemaining -= 1;
+  timer.innerText = secondsRemaining;
+  if (secondsRemaining === 0) {
+    clearInterval(timerIntervalID);
+  }
+};
+
+// const timerIntervalID = setInterval(decreaseTimer, 1000);
