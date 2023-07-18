7985ee77606122138e14b79067015f2acc7347db
Alessandro Fugazza <137590109+alessandrofugazza@users.noreply.github.com>
2023-07-18 12:06:40 +0200

    adds js to get answer from form

--- ale.js
+++ ale.js
@@ -13,3 +13,26 @@ const decreaseTimer = () => {
 };
 
 // const timerIntervalID = setInterval(decreaseTimer, 1000);
+
+const form = document.querySelector("form");
+
+const test = () => {
+  console.log("asdas");
+};
+
+// nextQuestionButton.addEventListener("click", test);
+
+const question = document.querySelector("h1");
+
+const answers = document.querySelectorAll("label");
+
+const [a1, a2, a3, a4] = answers;
+
+console.log(a1.innerText);
+
+const nextQuestion = e => {
+  e.preventDefault();
+  console.log(document.querySelector('input[name="answer"]:checked').value); // !todo fix
+};
+
+form.addEventListener("submit", nextQuestion);
