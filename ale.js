92ec5124e80858286f4d721c59b39340cc103b64
Alessandro Fugazza <137590109+alessandrofugazza@users.noreply.github.com>
2023-07-18 12:49:23 +0200

    adds next question function

--- ale.js
+++ ale.js
@@ -16,23 +16,38 @@ const decreaseTimer = () => {
 
 const form = document.querySelector("form");
 
-const test = () => {
-  console.log("asdas");
-};
-
 // nextQuestionButton.addEventListener("click", test);
 
-const question = document.querySelector("h1");
+const questionFrom = document.querySelector("h1");
 
-const answers = document.querySelectorAll("label");
+const answersForm = document.querySelectorAll("label");
 
-const [a1, a2, a3, a4] = answers;
-
-console.log(a1.innerText);
+const [a1, a2, a3, a4] = answersForm;
 
 const nextQuestion = e => {
   e.preventDefault();
   console.log(document.querySelector('input[name="answer"]:checked').value); // !todo fix
+  const nextQuestion = selectQuestion();
+  const answers = [nextQuestion.correct_answer, ...nextQuestion.incorrect_answers];
+  const randomizedAnswers = [];
+  for (let i = answers.length; i; i--) {
+    const randIndex = Math.floor(Math.random() * i - 1);
+    const answer = answers.splice(randIndex, 1);
+    randomizedAnswers.push(answer[0]);
+  }
+  questionFrom.innerText = nextQuestion.question;
+  for (let i = 0; i < randomizedAnswers.length; i++) {
+    answersForm[i].innerText = randomizedAnswers[i];
+  }
 };
 
 form.addEventListener("submit", nextQuestion);
+
+let remainingQuestions = questions.length;
+console.log(remainingQuestions);
+
+const selectQuestion = () => {
+  const nextQuestion = questions[Math.floor(Math.random()) * remainingQuestions];
+  console.log(nextQuestion);
+  return nextQuestion;
+};
