9b7892e21af7751451e7101ed478c21590a96a99
flaviodeangelis56 <deangelisflavio@gmail.com>
2023-07-19 13:29:50 +0200

    work-in-stars

--- feedback.js
+++ feedback.js
@@ -1,34 +1,46 @@
 const stars = document.querySelectorAll(".stars");
-
+// console.log(stars);
 // per click
-stars.forEach((stars) => {
-  stars.addEventListener("click", () => {
-    const rating = parseInt(stars.dataset.rating, 10); // ottenereil rating di proprietà "data-rating" della stella cliccata
+// stars.forEach((stars) => {
+//   stars.addEventListener("click", () => {
+//     const rating = parseInt(stars.dataset.rating, 10); // ottenereil rating di proprietà "data-rating" della stella cliccata
 
-    // Seleziona l'elemento input nascosto dove salvato  rating selezionato
-    const ratingInput = document.getElementById("ratingInput");
+// Seleziona l'elemento input nascosto dove salvato  rating selezionato
+// const ratingInput = document.getElementById("ratingInput");
 
-    // Imposta il rating selezionato nell'input nascosto
-    ratingInput.value = rating;
+// Imposta il rating selezionato nell'input nascosto
+// ratingInput.value = rating;
 
-    // Rimuovere la classe "active" da tutte le stelle
-    stars.forEach((s) => s.classList.remove("active"));
+// Rimuovere la classe "active" da tutte le stelle
+// stars.forEach((s) => s.classList.remove("active"));
 
-    // aggiongere la classe "active" per stelle rating selezionato
-    for (let i = 0; i < rating; i++) {
-      stars[i].classList.add("active");
+// aggiongere la classe "active" per stelle rating selezionato
+//     for (let i = 0; i < rating; i++) {
+//       stars[i].classList.add("active");
+//     }
+//   });
+// });
+let reatingUser = null;
+stars.forEach((singleStar, index) => {
+  singleStar.addEventListener("click", () => {
+    // console.log(index + 1);
+    for (let i = 0; i <= index; i++) {}
+    reatingUser = index + 1;
+    // return index + 1;
+    console.log(reatingUser);
+    for (let i = index + 1; i < reatingUser; i++) {
+      stars[i].classList.add("filter");
     }
   });
 });
-
 //"MORE INFO" per catturare il rating e il commento
-const moreInfoButton = document.querySelector(".inputButton");
-const commentInput = document.getElementById("commentInput");
+// const moreInfoButton = document.querySelector(".inputButton");
+// const commentInput = document.getElementById("commentInput");
 
-moreInfoButton.addEventListener("click", () => {
-  const rating = parseInt(document.getElementById("ratingInput").value, 10);
-  const comment = commentInput.value;
+// moreInfoButton.addEventListener("click", () => {
+//   const rating = parseInt(document.getElementById("ratingInput").value, 10);
+//   const comment = commentInput.value;
 
-  console.log(`Rating: ${rating}`);
-  console.log(`Commento: ${comment}`);
-});
+//   console.log(`Rating: ${rating}`);
+//   console.log(`Commento: ${comment}`);
+// });
--- result.js
+++ result.js
@@ -1,8 +1,42 @@
+// percentuali = percentuali.toString();
+
 const resetQuestions = () => {
   const main = document.getElementsByTagName("main");
+  // alert(`${correctAnswers}`);
+  let percentuali = Math.round((correctAnswers / totalQuestions) * 100 * 100) / 100;
+  console.log(percentuali);
   main[0].innerHTML = "";
+  main[0].innerHTML = `<img src=".\\assets\\img\\epicode_logo.png" class="epicode_logo" />
+  <article class="articleresults">
+    <h2 class="title">Results</h2>
+    <p class="summary">The summary of your answers:</p>
+    <div class="inblock correct">
+      <h3 class="corwro">Correct</h3>
+      <h3 class="corwro percentuali">${percentuali}%</h3>
+      <p>${correctAnswers}/${totalQuestions} questions</p>
+    </div>
+    <div class="inblock circle">
+      <h5 class="cong">Congratulations!</h5>
+      <h5 class="pass">You passed the exam.</h5>
+      <p class="send">
+        We'll send you the certificate<br />
+        in few minutes
+      </p>
+      <p class="email">
+        Check your email (including<br />
+        promotions/spam folder)
+      </p>
+    </div>
+    <div class="inblock wrong">
+      <h3 class="corwro">Wrong</h3>
+      <h3 class="corwro percentuali">${100 - percentuali}%</h3>
+      <p>${totalQuestions - correctAnswers}/${totalQuestions} questions</p>
+    </div>
+    <button type="button" class="button">RATE US</button>
+  </article>`;
+  const rateUs = document.getElementsByClassName("button");
+  // console.log(rateUs);
+  rateUs[0].addEventListener("click", () => {
+    window.location.href = "index.feedback.html";
+  });
 };
-
-// document.getElementById("result-button").addEventListener("click", function () {
-//   window.location.href = "index.feedback.html";
-// });
