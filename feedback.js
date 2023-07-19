de438bbc8206ba2632e79b3a493fc014fb8bb3cc
Alessandro Fugazza <137590109+alessandrofugazza@users.noreply.github.com>
2023-07-19 13:52:18 +0200

    fixes sending results

--- feedback.js
+++ feedback.js
@@ -1,25 +1,5 @@
 const stars = document.querySelectorAll(".stars");
-// console.log(stars);
-// per click
-// stars.forEach((stars) => {
-//   stars.addEventListener("click", () => {
-//     const rating = parseInt(stars.dataset.rating, 10); // ottenereil rating di proprietà "data-rating" della stella cliccata
 
-// Seleziona l'elemento input nascosto dove salvato  rating selezionato
-// const ratingInput = document.getElementById("ratingInput");
-
-// Imposta il rating selezionato nell'input nascosto
-// ratingInput.value = rating;
-
-// Rimuovere la classe "active" da tutte le stelle
-// stars.forEach((s) => s.classList.remove("active"));
-
-// aggiongere la classe "active" per stelle rating selezionato
-//     for (let i = 0; i < rating; i++) {
-//       stars[i].classList.add("active");
-//     }
-//   });
-// });
 let ratingUser = null;
 stars.forEach((singleStar, index) => {
   singleStar.addEventListener("click", () => {
@@ -33,18 +13,16 @@ stars.forEach((singleStar, index) => {
       stars[i].classList.remove("filter");
     }
     ratingUser = index + 1;
-    // return index + 1;
-    // console.log(ratingUser);
   });
 });
-//"MORE INFO" per catturare il rating e il commento
-// const moreInfoButton = document.querySelector(".inputButton");
-// const commentInput = document.getElementById("commentInput");
 
-// moreInfoButton.addEventListener("click", () => {
-//   const rating = parseInt(document.getElementById("ratingInput").value, 10);
-//   const comment = commentInput.value;
+// "MORE INFO" per catturare il rating e il commento
+const moreInfoButton = document.querySelector(".inputButton");
+const commentInput = document.querySelector(".inputText");
 
-//   console.log(`Rating: ${rating}`);
-//   console.log(`Commento: ${comment}`);
-// });
+moreInfoButton.addEventListener("click", () => {
+  const comment = commentInput.value;
+
+  console.log(`Rating: ${ratingUser}`);
+  console.log(`Commento: ${comment}`);
+});
