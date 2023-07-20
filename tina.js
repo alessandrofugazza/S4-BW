76592678f800e4f082a8593f9256c18745497178
Alessandro Fugazza <137590109+alessandrofugazza@users.noreply.github.com>
2023-07-20 14:05:31 +0200

    fixes checkbox

--- tina.js
+++ tina.js
@@ -2,10 +2,15 @@ function checkCheckbox() {
   const checkbox = document.getElementById("agree-checkbox");
   const proceedButton = document.querySelector(".custom-button");
   proceedButton.disabled = !checkbox.checked;
+  const fakeCheckbox = document.querySelector(".tick>div");
+  if (checkbox.checked) {
+    fakeCheckbox.parentElement.style.padding = "2px";
+  } else {
+    fakeCheckbox.parentElement.style.padding = "0px";
+  }
+  fakeCheckbox.classList.toggle("fakeTick");
 }
-window.onload = function () {
-  checkCheckbox(); // Controlla lo stato del checkbox al caricamento della pagina
-};
+
 document.getElementById("agree-checkbox").addEventListener("change", checkCheckbox);
 document.querySelector(".custom-button").addEventListener("click", function (event) {
   event.preventDefault(); // Evita il comportamento predefinito del pulsante (submit del form)
